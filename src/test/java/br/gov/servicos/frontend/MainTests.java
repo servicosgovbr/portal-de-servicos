8248e4cd6f1a4751179706a49dfde5c76ae057b3
Carlos Villela <cvillela@thoughtworks.com>
2015-02-02 15:46:26 -0200

    Inicialização com spring-boot - #1

---
+++ src/test/java/br/gov/servicos/frontend/MainTests.java
@@ -0,0 +1,17 @@
+package br.gov.servicos.frontend;
+
+import br.gov.servicos.frontend.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+public class MainTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
