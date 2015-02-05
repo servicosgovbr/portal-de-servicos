ff4fe84d86260a2e3bd0c992d0dcc9d4924f4ec4
Carlos Villela <cvillela@thoughtworks.com>
2015-02-05 19:43:32 -0200

    Adiciona métricas à home (acessados e ativados) - #30

--- src/main/java/br/gov/servicos/frontend/IndexController.java
+++ src/main/java/br/gov/servicos/frontend/IndexController.java
@@ -1,14 +1,32 @@
 package br.gov.servicos.frontend;
 
+import br.gov.servicos.servicos.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
 
 @Controller
 class IndexController {
 
+    ServicoRepository sr;
+
+    @Autowired
+    public IndexController(ServicoRepository sr) {
+        this.sr = sr;
+    }
+
     @RequestMapping("/")
-    String index() {
-        return "index";
+    ModelAndView index() {
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("acessos", sr.findAll(new PageRequest(0, 5, new Sort(Sort.Direction.DESC, "acessos"))));
+        model.put("ativacoes", sr.findAll(new PageRequest(0, 5, new Sort(Sort.Direction.DESC, "ativacoes"))));
+
+        return new ModelAndView("index", model);
     }
 
 }
--- src/test/java/br/gov/servicos/frontend/IndexControllerTest.java
+++ src/test/java/br/gov/servicos/frontend/IndexControllerTest.java
@@ -1,22 +1,52 @@
 package br.gov.servicos.frontend;
 
+import br.gov.servicos.servicos.Servico;
+import br.gov.servicos.servicos.ServicoRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.MockitoAnnotations.initMocks;
 
+@SuppressWarnings("unchecked")
 public class IndexControllerTest {
 
+    @Mock
+    ServicoRepository sr;
+    
     IndexController controller;
 
     @Before
     public void setUp() throws Exception {
-        controller = new IndexController();
+        initMocks(this);
+        controller = new IndexController(sr);
     }
 
     @Test
-    public void retornaViewAoAcessarIndex() throws Exception {
-        assertThat(controller.index(), is("index"));
+    public void retornaView() throws Exception {
+        assertThat(controller.index().getViewName(), is("index"));
+    }
+
+    @Test
+    public void retornaMetricasDeAcessoNoModel() throws Exception {
+        Page page = mock(Page.class);
+        given(sr.findAll(new PageRequest(0, 5, new Sort(Sort.Direction.DESC, "acessos")))).willReturn(page);
+        Page<Servico> actual = (Page<Servico>) controller.index().getModel().get("acessos");
+        assertThat(actual, is(page));
+    }
+
+    @Test
+    public void retornaMetricasDeAtivacaoNoModel() throws Exception {
+        Page page = mock(Page.class);
+        given(sr.findAll(new PageRequest(0, 5, new Sort(Sort.Direction.DESC, "ativacoes")))).willReturn(page);
+        Page<Servico> actual = (Page<Servico>) controller.index().getModel().get("ativacoes");
+        assertThat(actual, is(page));
     }
 }
