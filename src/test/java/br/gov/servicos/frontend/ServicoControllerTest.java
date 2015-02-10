45a7f2bb99273f0a9485db89f101016efc2cc9d9
Carlos Villela <cvillela@thoughtworks.com>
2015-02-10 15:08:34 -0200

    Extrai constante para o serviço

--- src/test/java/br/gov/servicos/frontend/ServicoControllerTest.java
+++ src/test/java/br/gov/servicos/frontend/ServicoControllerTest.java
@@ -1,5 +1,6 @@
 package br.gov.servicos.frontend;
 
+import br.gov.servicos.dominio.Orgao;
 import br.gov.servicos.dominio.Servico;
 import br.gov.servicos.dominio.ServicoRepository;
 import org.junit.Before;
@@ -13,6 +14,17 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ServicoControllerTest {
+
+    static final Servico SERVICO = new Servico(
+            "1",
+            "Título",
+            "Descrição",
+            "url://site",
+            "Gratuita",
+            new Orgao("Nome", "123"),
+            new Orgao("Nome", null),
+            0L, 0L
+    );
 
     @Mock
     ServicoRepository sr;
@@ -33,34 +45,25 @@ public class ServicoControllerTest {
 
     @Test
     public void buscaRetornaServicoNoModel() throws Exception {
-        Servico s1 = new Servico("1", "Como adicionar um novo emprego à sua Carteira de Trabalho", "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, " +
-                        "cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, " +
-                        "posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. " +
-                        "Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque " +
-                        "laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, " +
-                        "consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean massa.", null, null, null, null, 0L, 0L);
-
         ArgumentCaptor<Servico> captor = ArgumentCaptor.forClass(Servico.class);
 
-        given(sr.findOne("1")).willReturn(s1);
-        given(sr.save(captor.capture())).willReturn(s1);
+        given(sr.findOne("1")).willReturn(SERVICO);
+        given(sr.save(captor.capture())).willReturn(SERVICO);
 
         Servico actual = (Servico) controller.get("1").getModel().get("servico");
-        assertThat(actual, is(s1));
+        assertThat(actual, is(SERVICO));
         assertThat(captor.getValue().getAcessos(), is(1L));
     }
 
     @Test
     public void redirecionaUsuarioParaLinkDoServico() throws Exception {
-        Servico s1 = new Servico("1", "Como adicionar um novo emprego à sua Carteira de Trabalho", "desc", "URL://blah", null, null, null, 0L, 0L);
-
         ArgumentCaptor<Servico> captor = ArgumentCaptor.forClass(Servico.class);
 
-        given(sr.findOne("1")).willReturn(s1);
-        given(sr.save(captor.capture())).willReturn(s1);
+        given(sr.findOne("1")).willReturn(SERVICO);
+        given(sr.save(captor.capture())).willReturn(SERVICO);
 
         String actual = controller.navegar("1").getUrl();
-        assertThat(actual, is("URL://blah"));
+        assertThat(actual, is(SERVICO.getUrl()));
         assertThat(captor.getValue().getAtivacoes(), is(1L));
     }
 
