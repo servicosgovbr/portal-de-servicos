3c640634efa60ed9fe80d063f445216a19995cfd
Carlos Villela <cvillela@thoughtworks.com>
2015-02-19 14:16:15 -0200

    Muda o link das áreas de interesse e linhas da vida para buscas mais específicas (#55 e #57)
    De quebra, fica mais fácil entender o que o usuário queria na análise posterior.

--- src/main/java/br/gov/servicos/frontend/BuscaController.java
+++ src/main/java/br/gov/servicos/frontend/BuscaController.java
@@ -5,6 +5,8 @@ import br.gov.servicos.dominio.BuscaRepository;
 import br.gov.servicos.dominio.Servico;
 import br.gov.servicos.dominio.ServicoRepository;
 import org.elasticsearch.common.collect.Iterables;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.HashMap;
 
 import static org.elasticsearch.index.query.QueryBuilders.queryString;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 @Controller
 class BuscaController {
@@ -29,10 +32,25 @@ class BuscaController {
 
     @RequestMapping("/busca")
     ModelAndView busca(@RequestParam(required = true) String q) {
+        return doSearch(q, queryString(q));
+    }
+
+    @RequestMapping("/area-de-interesse")
+    ModelAndView areaDeInteresse(@RequestParam(required = true) String q) {
+        return doSearch(q, termQuery("areasDeInteresse", q));
+    }
+
+    @RequestMapping("/linha-da-vida")
+    ModelAndView linhaDaVida(@RequestParam(required = true) String q) {
+        TermQueryBuilder linhasDaVida;
+        return doSearch(q, termQuery("linhasDaVida", q));
+    }
+
+    private ModelAndView doSearch(String q, QueryBuilder query) {
         HashMap<String, Object> model = new HashMap<>();
         model.put("termo", q);
 
-        Iterable<Servico> servicos = sr.search(queryString(q));
+        Iterable<Servico> servicos = sr.search(query);
         model.put("resultados", servicos);
 
         Busca busca = br.findOne(q);
