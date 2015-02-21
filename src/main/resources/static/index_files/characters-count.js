563782f41651031f4bd4c6ff7ca170cf46db8066
Carlos Villela <cvillela@thoughtworks.com>
2015-02-21 00:48:45 -0200

    Remove characters-count.js (não usado)

--- src/main/resources/static/index_files/characters-count.js
+++
@@ -1,3 +0,0 @@
-
-/* - characters-count.js - */
-$(document).ready(function() {$("#form-widgets-IDublinCore-title").charCount({"optimal": 100, "counterText": "Characters left: ", "allowed": 100}); $("#form-widgets-IDublinCore-description").charCount({"optimal": 200, "counterText": "Characters left: ", "allowed": 100});});
