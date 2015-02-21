1509e84693e931f08978a65c34a923611eaa38fb
Carlos Villela <cvillela@thoughtworks.com>
2015-02-21 16:44:22 -0200

    Atualiza jQuery, jQuery.Cookie, remove plone_javascript_variables.js (não usado)

--- src/main/resources/static/index_files/plone_javascript_variables.js
+++
@@ -1,9 +0,0 @@
-
-/* - plone_javascript_variables.js - */
-var portal_url = '';
-var form_modified_message = 'O seu formulário não foi salvo. Todas as alterações serão perdidas.';
-var form_resubmit_message = 'Você já clicou no botão de envio. Você realmente quer enviar este formulário novamente?';
-var external_links_open_new_window = 'false';
-var mark_special_links = 'false';
-var ajax_noresponse_message = 'Nenhuma resposta do servidor. Por favore, tente mais tarde.';
-
