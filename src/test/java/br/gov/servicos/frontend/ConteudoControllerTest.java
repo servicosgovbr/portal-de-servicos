8118e98fcc9ed5b0cac4df6366736029a4a94ca8
Erick Pintor <erickpintor@gmail.com>
2015-02-24 18:08:36 -0300

    Refatora testes para ConteudoController

--- src/test/java/br/gov/servicos/frontend/ConteudoControllerTest.java
+++ src/test/java/br/gov/servicos/frontend/ConteudoControllerTest.java
@@ -2,42 +2,47 @@ package br.gov.servicos.frontend;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.core.io.ClassPathResource;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
-import static org.mockito.MockitoAnnotations.initMocks;
+import java.io.IOException;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeValue;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ConteudoControllerTest {
 
     @Mock
-    Markdown markdown;
-
-    ConteudoController controller;
+    private Markdown markdown;
+    private ConteudoController controller;
 
     @Before
-    public void setUp() throws Exception {
-        initMocks(this);
+    public void setUp() {
         controller = new ConteudoController(markdown);
     }
 
+    @Test
+    public void redirecionaParaAPaginaDeConteudo() throws NotFoundException, IOException {
+        assertViewName(controller.conteudo("pagina"), "conteudo");
+    }
+
     @Test(expected = NotFoundException.class)
-    public void retorna404QuandoArquivoNaoExiste() throws Exception {
+    public void retorna404QuandoArquivoNaoExiste() throws NotFoundException, IOException {
         controller.conteudo("arquivo-nao-existente");
     }
 
     @Test
-    public void retornaView() throws Exception {
-        assertThat(controller.conteudo("pagina").getViewName(), is("conteudo"));
+    public void compilaOMarkdownDoConteudo() throws NotFoundException, IOException {
+        doReturn("<h1>Conteúdo</h1>")
+                .when(markdown)
+                .toHtml(any(ClassPathResource.class));
+
+        assertModelAttributeValue(controller.conteudo("pagina"), "conteudo", "<h1>Conteúdo</h1>");
     }
 
-    @Test
-    public void retornaHtmlNoModel() throws Exception {
-        given(markdown.toHtml(any(ClassPathResource.class))).willReturn("<h1>Conteúdo</h1>");
-        assertThat(controller.conteudo("pagina").getModel().get("conteudo"), is("<h1>Conteúdo</h1>"));
-    }
-    
 }
