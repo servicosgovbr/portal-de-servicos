df1c490fc37583fdc5649dd4c7781ad4526bf310
Carlos Villela <cvillela@thoughtworks.com>
2015-02-25 18:50:34 -0300

    Usa SPEL para extrair dados

--- src/main/java/br/gov/servicos/legado/ServicoTypeMapper.java
+++ src/main/java/br/gov/servicos/legado/ServicoTypeMapper.java
@@ -5,15 +5,14 @@ import br.gov.servicos.servico.Servico;
 import com.github.slugify.Slugify;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.Expression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 import static lombok.AccessLevel.PRIVATE;
 
 @Component
@@ -21,19 +20,11 @@ import static lombok.AccessLevel.PRIVATE;
 class ServicoTypeMapper implements Function<ServicoType, Servico> {
     Slugify slugify;
 
-    Expression getLinhasDaVida;
+    SpelExpressionParser parser = new SpelExpressionParser();
 
     @Autowired
     public ServicoTypeMapper(Slugify slugify) {
         this.slugify = slugify;
-
-        SpelExpressionParser parser = new SpelExpressionParser();
-
-        getLinhasDaVida = parser.parseExpression("(" +
-                "publicosAlvo?.content.size() == 0 ? " +
-                "  null : " +
-                "  publicosAlvo?.content?.get(0).value?.linhasDaViva?.linhaDaVida" +
-                ")?:{}");
     }
 
     @Override
@@ -54,66 +45,54 @@ class ServicoTypeMapper implements Function<ServicoType, Servico> {
         );
     }
 
+    @SuppressWarnings("unchecked")
     private List<String> linhasDaVida(ServicoType servicoType) {
-        @SuppressWarnings("unchecked")
-        List<LinhaDaVidaType> linhaDaVida = (List<LinhaDaVidaType>) getLinhasDaVida.getValue(servicoType);
-
-        return linhaDaVida.stream()
-                .map(LinhaDaVidaType::getTitulo)
-                .collect(toList());
+        return ((List<List<String>>)
+                parser.parseExpression(
+                        "publicosAlvo?.content.![value?.linhasDaViva?.linhaDaVida.![titulo]]?:{}")
+                        .getValue(servicoType))
+                .stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
-    private static Orgao orgaoPrestador(ServicoType servicoType) {
-        OrgaoPrestadorType prestador = servicoType.getOrgaoPrestador();
-        String nome = prestador == null ? null : prestador.getTitulo();
-        String telefone = prestador == null ? null : prestador.getTelefone();
-
-        return new Orgao(nome, telefone);
+    private Orgao orgaoPrestador(ServicoType servicoType) {
+        return parser.parseExpression(
+                "new br.gov.servicos.servico.Orgao(orgaoPrestador?.titulo, orgaoPrestador?.telefone)")
+                .getValue(servicoType, Orgao.class);
     }
 
-    private static Orgao orgaoResponsavel(ServicoType servicoType) {
-        OrgaoResponsavelType responsavel = servicoType.getOrgaoResponsavel();
-        return responsavel == null ? null : new Orgao(responsavel.getTitulo(), null);
+    private Orgao orgaoResponsavel(ServicoType servicoType) {
+        return parser.parseExpression(
+                "new br.gov.servicos.servico.Orgao(orgaoResponsavel?.titulo, null)")
+                .getValue(servicoType, Orgao.class);
     }
 
+    @SuppressWarnings("unchecked")
     private List<String> eventosDasLinhasDaVida(ServicoType servicoType) {
-        @SuppressWarnings("unchecked")
-        List<LinhaDaVidaType> linhaDaVida = (List<LinhaDaVidaType>) getLinhasDaVida.getValue(servicoType);
-
-        return linhaDaVida.stream()
-                .flatMap((linhaDaVidaType) -> {
-                    EventoslinhaDaVidaType eventoslinhaDaVida = linhaDaVidaType.getEventoslinhaDaVida();
-                    if (eventoslinhaDaVida == null) return null;
-
-                    List<EventolinhaDaVidaType> eventolinhaDaVida = eventoslinhaDaVida.getEventolinhaDaVida();
-                    if (eventolinhaDaVida == null) return null;
-
-                    return eventolinhaDaVida.stream().map(EventolinhaDaVidaType::getTitulo);
-                })
-                .collect(toList());
-
+        return ((List<List<List<String>>>)
+                parser.parseExpression(
+                        "publicosAlvo?.content.![value?.linhasDaViva?.linhaDaVida.![eventoslinhaDaVida?.eventolinhaDaVida?.![titulo]]]?:{}")
+                        .getValue(servicoType))
+                .stream()
+                .flatMap(Collection::stream)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
-    private static List<String> areasDeInteresse(ServicoType servicoType) {
-        AreasInteresseType areasInteresseType = servicoType.getAreasInteresse();
-        if (areasInteresseType == null) {
-            return Arrays.asList();
-        }
-
-        return areasInteresseType.getArea().stream().map(AreaType::getTitulo).collect(toList());
+    @SuppressWarnings("unchecked")
+    private List<String> areasDeInteresse(ServicoType servicoType) {
+        return parser.parseExpression(
+                "areasInteresse?.area?.![titulo]?:{}")
+                .getValue(servicoType, List.class);
     }
 
-    private static String url(ServicoType servicoType) {
-        if (servicoType.getCanaisPrestacaoServico() == null || servicoType.getCanaisPrestacaoServico().getCanalPrestacaoServico() == null) {
-            return null;
-        }
+    @SuppressWarnings("unchecked")
+    private String url(ServicoType servicoType) {
+        List<String> urls = parser.parseExpression(
+                "canaisPrestacaoServico?.canalPrestacaoServico?.![url]?:{}")
+                .getValue(servicoType, List.class);
 
-        for (CanalPrestacaoServicoType canal : servicoType.getCanaisPrestacaoServico().getCanalPrestacaoServico()) {
-            if (canal.getUrl() != null) {
-                return canal.getUrl();
-            }
-        }
-
-        return null;
+        return urls.isEmpty() ? null : urls.get(0);
     }
 }
--- src/main/java/br/gov/servicos/servico/Orgao.java
+++ src/main/java/br/gov/servicos/servico/Orgao.java
@@ -3,7 +3,6 @@ package br.gov.servicos.servico;
 import lombok.Value;
 import org.springframework.data.elasticsearch.annotations.Field;
 
-import static org.elasticsearch.common.Strings.isEmpty;
 import static org.springframework.data.elasticsearch.annotations.FieldType.String;
 
 @Value
@@ -20,7 +19,7 @@ public class Orgao {
     }
 
     public Orgao(String nome, String telefone) {
-        this.nome = isEmpty(nome) ? null : nome;
-        this.telefone = isEmpty(telefone) ? null : telefone;
+        this.nome = nome;
+        this.telefone = telefone;
     }
 }
