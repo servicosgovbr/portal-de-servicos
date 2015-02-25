1d7f0b7439281e88beead064dcbbb6424fb07c94
Erick Pintor <erickpintor@gmail.com>
2015-02-25 15:11:39 -0300

    Refatorando RobotsController

--- src/main/java/br/gov/servicos/frontend/RobotsTxtController.java
+++ src/main/java/br/gov/servicos/frontend/RobotsTxtController.java
@@ -1,19 +1,20 @@
 package br.gov.servicos.frontend;
 
-import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static lombok.AccessLevel.PRIVATE;
+import static org.springframework.http.HttpStatus.OK;
+
 @Controller
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 public class RobotsTxtController {
 
     @RequestMapping(value = "/robots.txt", produces = "text/plain")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     @ResponseBody
     public String robotsTxt() {
         return "User-agent: *\n" +
