501d7f26d013e868d3fcf9d248df6af94c113ae6
Carlos Villela <cvillela@thoughtworks.com>
2015-02-25 16:40:34 -0300

    Adiciona Slugify ao projeto - #70

---
+++ src/main/java/br/gov/servicos/config/SlugifyConfig.java
@@ -0,0 +1,16 @@
+package br.gov.servicos.config;
+
+import com.github.slugify.Slugify;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class SlugifyConfig {
+
+    @Bean
+    public Slugify slugify() throws IOException {
+        return new Slugify();
+    }
+}
