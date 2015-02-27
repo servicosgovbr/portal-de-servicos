d35c68263754f30c68e25bfbe9ba10a2dbc8283d
Carlos Villela <cvillela@thoughtworks.com>
2015-02-27 11:52:28 -0300

    Permite instanciar exception passando uma causa

--- src/main/java/br/gov/servicos/foundation/exceptions/ConteudoNaoEncontrado.java
+++ src/main/java/br/gov/servicos/foundation/exceptions/ConteudoNaoEncontrado.java
@@ -2,11 +2,19 @@ package br.gov.servicos.foundation.exceptions;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.IOException;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ResponseStatus(value = NOT_FOUND, reason = "Página não encontrada")
-public class ConteudoNaoEncontrado extends Exception {
+public class ConteudoNaoEncontrado extends RuntimeException {
 
     private static final long serialVersionUID = -7299858067001144911L;
 
+    public ConteudoNaoEncontrado() {
+    }
+
+    public ConteudoNaoEncontrado(IOException cause) {
+        super(cause);
+    }
 }
