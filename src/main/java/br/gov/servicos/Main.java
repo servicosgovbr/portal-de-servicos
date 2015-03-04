d43349a4e10dead3e4eaa94c4b4f4fa8bd83c1be
Erick Pintor <erickpintor@gmail.com>
2015-03-04 14:44:11 -0300

    Revertendo "Adiciona asset pipeline a aplicação já com suporte a less - #59" para resolver testes de integração que quebraram
    This reverts commit fe3f3de5863418a8cf37e15c512d54c5bc5ff0df.

--- src/main/java/br/gov/servicos/Main.java
+++ src/main/java/br/gov/servicos/Main.java
@@ -2,12 +2,10 @@ package br.gov.servicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
 @EnableElasticsearchRepositories
-@ComponentScan({"br.gov.servicos", "asset.pipeline"})
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
