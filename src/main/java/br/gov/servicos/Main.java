fe3f3de5863418a8cf37e15c512d54c5bc5ff0df
Erick Pintor <erickpintor@gmail.com>
2015-03-04 14:07:35 -0300

    Adiciona asset pipeline a aplicação já com suporte a less - #59

--- src/main/java/br/gov/servicos/Main.java
+++ src/main/java/br/gov/servicos/Main.java
@@ -2,10 +2,12 @@ package br.gov.servicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories
 @SpringBootApplication
+@EnableElasticsearchRepositories
+@ComponentScan({"br.gov.servicos", "asset.pipeline"})
 public class Main {
 
     public static void main(String[] args) {
