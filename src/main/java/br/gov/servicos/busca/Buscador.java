7a92198c45d08928402662edd7d181387bdd978d
Carlos Villela <cvillela@thoughtworks.com>
2015-03-07 00:23:16 -0300

    Adiciona buscas fuzzy para tentar extrair mais resultados. Alguns exemplos de erros de ortografia passaram a funcionar (cdeula, pnesao etc) - #86

--- src/main/java/br/gov/servicos/busca/Buscador.java
+++ src/main/java/br/gov/servicos/busca/Buscador.java
@@ -4,8 +4,8 @@ import br.gov.servicos.servico.Servico;
 import br.gov.servicos.servico.ServicoRepository;
 import lombok.experimental.FieldDefaults;
 import org.elasticsearch.common.collect.Lists;
+import org.elasticsearch.index.query.FuzzyQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +16,8 @@ import java.util.function.Function;
 
 import static java.util.Collections.unmodifiableList;
 import static lombok.AccessLevel.PRIVATE;
-import static org.elasticsearch.index.query.QueryBuilders.fuzzyLikeThisQuery;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.common.unit.Fuzziness.TWO;
+import static org.elasticsearch.index.query.QueryBuilders.*;
 
 @Component
 @FieldDefaults(level = PRIVATE, makeFinal = true)
@@ -35,7 +35,21 @@ class Buscador {
     }
 
     List<Servico> busca(Optional<String> termoBuscado) {
-        return executaQuery(termoBuscado, QueryBuilders::queryString);
+        return executaQuery(termoBuscado, (q) -> disMaxQuery()
+                        .add(queryString(q).boost(5f))
+                        .add(boolQuery()
+                                .should(fuzzy(q, "titulo", 1.0f))
+                                .should(fuzzy(q, "descricao", 0.9f))
+                        )
+        );
+    }
+
+    private FuzzyQueryBuilder fuzzy(String q, String field, float boost) {
+        return fuzzyQuery(field, q)
+                .boost(boost)
+                .fuzziness(TWO)
+                .prefixLength(0)
+                .transpositions(true);
     }
 
     List<Servico> buscaPor(String campo, Optional<String> termoBuscado) {
