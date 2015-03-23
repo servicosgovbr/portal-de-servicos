f7b7baf564c63faca514ff334ca153f4c6afe069
Jean Kirchner <jean.kirchner@gmail.com>
2015-03-23 19:25:34 -0300

    Provisiona Vagrant com Java 8, e Elastic Search 1.5.0, #69

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network "forwarded_port", guest: 80, host: 8081
   config.ssh.forward_agent = true
 
+  config.vm.provision :shell, path: "vagrant/bootstrap.sh"
+
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--memory", "2048"]
   end
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,16 @@
 sudo -v
 
 echo "Atualizando repositório Yum"
-sudo yum makecache fast
-sudo yum update
+sudo yum makecache fast -y
+sudo yum update -y
 
+mkdir gds
+pushd gds
+  echo "Instalando Java 8"
+  wget -c --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.rpm"
+  sudo rpm -Uvh jdk-8u40-linux-x64.rpm
+  java -version
+
+  echo "Instalando ElasticSearch"
+  sudo rpm -Uvh https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.0.noarch.rpm
+popd
