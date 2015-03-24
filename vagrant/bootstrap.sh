546ee004d4a1d83ea340c191baa36e6596c08b31
Carlos Villela <cvillela@thoughtworks.com>
2015-03-24 08:23:37 -0300

    Não instala os RPMs se já forem instalados - #69

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -9,7 +9,7 @@ pushd 'gds'
   if [ -f 'jdk-8u40-linux-x64.rpm' ]; then
     echo 'Java 8 já foi baixado'
   else
-    echo 'Baixando Java 8'
+    echo 'Baixando JDK'
     wget -c \
       --quiet \
       --no-cookies \
@@ -18,13 +18,24 @@ pushd 'gds'
       'http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.rpm'
   fi
 
-  echo 'Instalando Java 8'
-  rpm -Uvh 'jdk-8u40-linux-x64.rpm'
+  (rpm -qa | grep -q jdk1.8.0_40-1.8.0_40-fcs.x86_64) || \
+    (echo 'Instalando JDK' && rpm -Uvh 'jdk-8u40-linux-x64.rpm')
 
-  $(java -version 2>&1 | grep -q "1.8.0_42") || (echo "Java 8 não foi instalado corretamente" && exit -1)
+  $(java -version 2>&1 | grep -q "1.8.0_42") || \
+    (echo "JDK não foi instalada corretamente" && exit -1)
 
-  echo 'Instalando ElasticSearch'
-  rpm -Uvh https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.0.noarch.rpm
+  if [ -f elasticsearch-1.5.0.noarch.rpm ]; then
+    echo 'ElasticSearch já foi baixado'
+  else
+    echo 'Baixando ElasticSearch'
+    wget -c \
+      --quiet \
+      --no-check-certificate \
+      https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.0.noarch.rpm
+  fi
+
+  (rpm -qa | grep -q elasticsearch-1.5.0-1.noarch) || \
+    (echo 'Instalando ElasticSearch' && rpm -Uvh elasticsearch-1.5.0.noarch.rpm)
 
   systemctl daemon-reload
   systemctl enable elasticsearch.service
