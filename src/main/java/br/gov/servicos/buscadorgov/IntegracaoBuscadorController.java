0e1ccec8d155e90edaceb3bcc30fc3caf1ad53ae
Carlos Villela <cvillela@thoughtworks.com>
2015-03-24 11:15:30 -0300

    Simplifica geração de XML para o buscador govbr

--- src/main/java/br/gov/servicos/buscadorgov/IntegracaoBuscadorController.java
+++ src/main/java/br/gov/servicos/buscadorgov/IntegracaoBuscadorController.java
@@ -3,14 +3,10 @@ package br.gov.servicos.buscadorgov;
 import br.gov.servicos.servico.ServicoRepository;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.FacetedPageImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 import static lombok.AccessLevel.PRIVATE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -29,17 +25,14 @@ class IntegracaoBuscadorController {
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/servicos", produces = "text/xml", method = GET)
-    @ResponseBody
-    ResultadoListarServicos get(HttpServletRequest request) {
-        FacetedPageImpl all = (FacetedPageImpl) servicos.findAll();
-        return new ResultadoListarServicos(((List<br.gov.servicos.servico.Servico>) all.getContent()), request);
+    ModelAndView get() {
+        return new ModelAndView("resultado-listar-servicos", "servicos", servicos.findAll());
     }
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/servico/{id}", produces = "text/xml", method = GET)
-    @ResponseBody
-    ResultadoDetalharServico get(@PathVariable("id") String id) {
-        return new ResultadoDetalharServico(servicos.findOne(id));
+    ModelAndView get(@PathVariable("id") String id) {
+        return new ModelAndView("resultado-detalhar-servico", "servico", servicos.findOne(id));
     }
 
 }
--- src/test/java/br/gov/servicos/buscadorgov/IntegracaoBuscadorControllerTest.java
+++ src/test/java/br/gov/servicos/buscadorgov/IntegracaoBuscadorControllerTest.java
@@ -7,15 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.data.elasticsearch.core.FacetedPageImpl;
-import org.springframework.mock.web.MockHttpServletRequest;
 
-import static br.gov.servicos.fixtures.TestData.SERVICO;
-import static java.util.Arrays.asList;
 import static lombok.AccessLevel.PRIVATE;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 
 @RunWith(MockitoJUnitRunner.class)
 @FieldDefaults(level = PRIVATE)
@@ -33,27 +28,14 @@ public class IntegracaoBuscadorControllerTest {
 
     @Test
     public void retornaListaDeLinksParaServicos() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setServerName("foo");
-        request.setServerPort(80);
-
-        given(servicos.findAll())
-                .willReturn(new FacetedPageImpl<>(asList(SERVICO)));
-
-        ResultadoListarServicos listaDeServicos = controller.get(request);
-
-        assertThat(listaDeServicos.getListaServicos(),
-                is(not(emptyCollectionOf(Servico.class))));
+        assertViewName(controller.get(), "resultado-listar-servicos");
+        verify(servicos).findAll();
     }
 
     @Test
     public void retornaDetalheDeServico() throws Exception {
-        br.gov.servicos.servico.Servico servico = SERVICO.withId("svc");
-
-        given(servicos.findOne("svc")).willReturn(servico);
-
-        ResultadoDetalharServico r = controller.get("svc");
-        assertThat(r.getServico(), is(servico));
+        assertViewName(controller.get("svc"), "resultado-detalhar-servico");
+        verify(servicos).findOne("svc");
     }
 
 }
