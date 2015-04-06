58e6150d3ddc89dd28a70fcbb9c9a6b46e7fa65d
Carlos Villela <cvillela@thoughtworks.com>
2015-04-06 13:29:25 -0300

    Adiciona logs à chamada ao piwik - #150

--- src/main/java/br/gov/servicos/piwik/PiwikClient.java
+++ src/main/java/br/gov/servicos/piwik/PiwikClient.java
@@ -2,13 +2,18 @@ package br.gov.servicos.piwik;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
+
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PiwikClient {
     RestTemplate restTemplate;
@@ -25,23 +30,26 @@ public class PiwikClient {
     }
 
     public List<PiwikPage> getPageUrls(String period, String date) {
-        return Arrays.asList(
-                restTemplate.getForEntity(
-                        buildCommonParams()
-                                .queryParam("method", "Actions.getPageUrls")
-                                .queryParam("period", period)
-                                .queryParam("date", date)
-                                .build()
-                                .toUri(),
-                        PiwikPage[].class)
-                        .getBody());
+        URI uri = buildURI(period, date);
+
+        log.debug("Fazendo requisição ao Piwik: {}", uri);
+        ResponseEntity<PiwikPage[]> entity = restTemplate.getForEntity(uri, PiwikPage[].class);
+        log.debug("Resposta do Piwik: {}", entity.getStatusCode());
+
+        return asList(entity.getBody());
     }
 
-    private UriComponentsBuilder buildCommonParams() {
-        return UriComponentsBuilder.fromHttpUrl(url)
+    private URI buildURI(String period, String date) {
+        return fromHttpUrl(url)
                 .queryParam("module", "API")
                 .queryParam("format", "json")
                 .queryParam("idSite", site)
-                .queryParam("token_auth", authToken);
+                .queryParam("token_auth", authToken)
+                .queryParam("method", "Actions.getPageUrls")
+                .queryParam("period", period)
+                .queryParam("date", date)
+                .build()
+                .toUri();
     }
+
 }
