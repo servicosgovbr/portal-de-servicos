f957a9f43a74a570163559d7436572e289e63862
Erick Pintor <erickpintor@gmail.com>
2015-04-10 15:38:37 -0300

    Divide carta de servicos em serviço, benefício ou dever - servicosgovbr/editor-guia-servicos#1

--- src/test-integration/java/br/gov/servicos/cartaServicos/VersoesTest.java
+++ src/test-integration/java/br/gov/servicos/cartaServicos/VersoesTest.java
@@ -2,79 +2,72 @@ package br.gov.servicos.cartaServicos;
 
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-import static java.lang.String.format;
-import static java.nio.file.Files.newDirectoryStream;
 import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
 import static lombok.AccessLevel.PRIVATE;
 import static org.junit.Assert.fail;
 
-@Slf4j
 @FieldDefaults(level = PRIVATE)
 public class VersoesTest {
 
-    private static final String PASTA_DE_CARTAS_SERVICOS = "/cartaServicos/";
-    private static final String VERSAO_CARTA = ".*/v\\d+$";
+    private static final String CARTA_SERVICOS = "/cartaServicos/";
+    private static final String ARQUIVO_XSD = ".*/v\\d+/\\w+\\.xsd";
+    private static final String XSD = ".xsd";
+    private static final String EXEMPLO_XSL = "-exemplo.xml";
 
     SchemaFactory validadorXsd;
-    Path cartasServico;
+    Path cartaServicos;
 
     @Before
     public void setUp() throws Exception {
         validadorXsd = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI);
         validadorXsd.setResourceResolver(new ClassPathResolver());
 
-        cartasServico = Paths.get(resource(PASTA_DE_CARTAS_SERVICOS).toURI());
+        cartaServicos = Paths.get(
+                getClass().getResource(CARTA_SERVICOS).toURI()
+        );
     }
 
     @Test
     public void deveValidarExemplosContraXSDs() throws Exception {
-        Iterator<Path> pastas = newDirectoryStream(cartasServico, p -> p.toString().matches(VERSAO_CARTA)).iterator();
-        if (!pastas.hasNext()) {
+        Iterator<Path> xsds = Files.walk(cartaServicos)
+                .filter(p -> p.toString().matches(ARQUIVO_XSD))
+                .iterator();
+
+        if (!xsds.hasNext()) {
             fail("Nenhuma versão de XSD foi encontrada para ser validada");
         }
 
-        pastas.forEachRemaining(this::validaExemplo);
+        xsds.forEachRemaining(this::validaExemplo);
     }
 
     @SneakyThrows
     private void validaExemplo(Path versao) {
-        log.info("Validando XSD e exemplo XML em: {}", versao);
-
         Validator validador = validadorXsd
-                .newSchema(xsd(versao))
+                .newSchema(versao.toUri().toURL())
                 .newValidator();
 
         validador.validate(xml(versao));
     }
 
-    private URL xsd(Path versao) {
-        return versaoDe("carta.xsd", versao);
-    }
-
     private StreamSource xml(Path versao) throws IOException {
-        InputStream xml = versaoDe("exemplo.xml", versao).openStream();
+        String arquivoExemplo = versao.toString().replace(XSD, EXEMPLO_XSL);
+        InputStream xml = new FileInputStream(arquivoExemplo);
+
         return new StreamSource(xml);
     }
 
-    private URL versaoDe(String arquivo, Path versao) {
-        return resource("%s/%s/%s", PASTA_DE_CARTAS_SERVICOS, versao.getFileName(), arquivo);
-    }
-
-    private URL resource(String path, Object... args) {
-        return getClass().getResource(format(path, args));
-    }
 }
