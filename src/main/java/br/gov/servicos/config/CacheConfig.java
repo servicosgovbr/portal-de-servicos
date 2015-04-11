cc518546c06848caf83c8d147990db0812545c61
Carlos Villela <cvillela@thoughtworks.com>
2015-04-11 08:16:49 -0300

    Deixa o Spring listar os caches - #150

--- src/main/java/br/gov/servicos/config/CacheConfig.java
+++ src/main/java/br/gov/servicos/config/CacheConfig.java
@@ -1,6 +1,7 @@
 package br.gov.servicos.config;
 
 import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCache;
@@ -8,20 +9,25 @@ import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
     @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        GuavaCache piwiki = new GuavaCache("piwik", CacheBuilder
+    public CacheManager cacheManager(List<Cache> caches) {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+
+    @Bean
+    public Cache piwikCache() {
+        return new GuavaCache("piwik", CacheBuilder
                 .newBuilder()
                 .expireAfterWrite(1, TimeUnit.DAYS)
                 .build());
-        simpleCacheManager.setCaches(Arrays.asList(piwiki));
-        return simpleCacheManager;
     }
 }
