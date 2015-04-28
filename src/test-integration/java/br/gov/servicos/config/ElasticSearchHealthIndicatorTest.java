e733ce0b6c85958c2502a2e0c72d38beb4686328
Carlos Villela <cvillela@thoughtworks.com>
2015-04-28 10:20:50 -0300

    Adiciona teste de integração simples para o ESHI

---
+++ src/test-integration/java/br/gov/servicos/config/ElasticSearchHealthIndicatorTest.java
@@ -0,0 +1,24 @@
+package br.gov.servicos.config;
+
+import br.gov.servicos.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+public class ElasticSearchHealthIndicatorTest {
+
+    @Autowired
+    ElasticSearchHealthIndicator healthIndicator;
+
+    @Test
+    public void indicaStatusDoCluster() throws Exception {
+        assertThat(healthIndicator.health().getStatus().getCode(), is("UP"));
+    }
+}
