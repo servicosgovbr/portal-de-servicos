8076b1a7a4b3ba856b679ee0d9e0aed07147585c
Erick Pintor <erickpintor@gmail.com>
2015-05-04 12:40:46 -0300

    Remove classes e código javascript legado e não utilizado

--- src/main/resources/static/assets/main.js
+++ src/main/resources/static/assets/main.js
@@ -1,14 +1,15 @@
 jQuery(document).ready(function($) {
 
   $('.portletNavigationTree .portletHeader').click(function() {
-    $(this).next().slideToggle();
-    $(this).find('i.fa').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    $(this).next()
+		.slideToggle();
+
+    $(this).find('i.fa')
+		.toggleClass('fa-chevron-up')
+		.toggleClass('fa-chevron-down');
   });
 
-  $('ul li:last-child').addClass('last-item');
-  $('#portal-column-one div:first-child').addClass('first-item');
-
-  $('form').each(function(_, form) {
-    $(form).validate();
+  $('form').each(function(_, f) {
+    $(f).validate();
   });
 });
