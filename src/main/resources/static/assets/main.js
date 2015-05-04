90268e8ed37b1a4058c1ac7f6a166903629841c3
Erick Pintor <erickpintor@gmail.com>
2015-05-04 12:36:13 -0300

    Remove código legado para responsividade

--- src/main/resources/static/assets/main.js
+++ src/main/resources/static/assets/main.js
@@ -6,50 +6,6 @@ jQuery(document).ready(function($) {
   });
 
   $('ul li:last-child').addClass('last-item');
-  
-  var responsiveResize, root;
-  root = typeof exports !== 'undefined' && exports !== null ? exports : this;
-  root.ResponsiveResize = function() {
-    var _Singleton, _base;
-    _Singleton = function() {
-      function _Singleton() {}
-      _Singleton.prototype.perspectiva_anterior = '';
-      _Singleton.prototype.scrollbar = false;
-      _Singleton.prototype.resize = function() {
-        var perspectiva_atual;
-        if ($(window).width() <= 480) {
-          perspectiva_atual = 'mobile';
-        } else {
-          perspectiva_atual = 'desktop';
-        }
-        if (this.perspectiva_anterior !== perspectiva_atual) {
-          this.perspectiva_anterior = perspectiva_atual;
-          if (perspectiva_atual === 'mobile') {
-            $('body').addClass('mobile');
-          } else {
-            $('body').removeClass('mobile');
-          }
-        }
-      };
-      return _Singleton;
-    }();
-    if ((_base = root.ResponsiveResize).instance == null) {
-      _base.instance = new _Singleton();
-    }
-    return root.ResponsiveResize.instance;
-  };
-
-  var resize = function() {
-    responsiveResize = new root.ResponsiveResize();
-    responsiveResize.resize();
-  };
-
-  $(window).resize(function() {
-    resize();
-  });
-
-  resize();
-
   $('#portal-column-one div:first-child').addClass('first-item');
 
   $('form').each(function(_, form) {
