708a5184a9d95d423a2d34dfb50a29d4e69a15fe
Carlos Villela <cvillela@thoughtworks.com>
2015-05-07 13:56:37 -0300

    Conserta teste

--- src/test/java/br/gov/servicos/frontend/RobotsTxtControllerTest.java
+++ src/test/java/br/gov/servicos/frontend/RobotsTxtControllerTest.java
@@ -1,18 +1,16 @@
 package br.gov.servicos.frontend;
 
-import br.gov.servicos.foundation.http.BaseUrl;
 import lombok.experimental.FieldDefaults;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 import static java.util.Arrays.asList;
 import static lombok.AccessLevel.PRIVATE;
 import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
 
 @FieldDefaults(level = PRIVATE)
 @RunWith(MockitoJUnitRunner.class)
@@ -20,25 +18,26 @@ public class RobotsTxtControllerTest {
 
     RobotsTxtController controller;
 
-    @Mock
-    BaseUrl baseUrl;
+    MockHttpServletRequest request = new MockHttpServletRequest();
 
     @Before
     public void setUp() throws Exception {
         controller = new RobotsTxtController();
 
-        given(baseUrl.and("/sitemap.xml")).willReturn("http://servicos.gov.br/sitemap.xml");
+        request.setScheme("http");
+        request.setServerName("servicos.gov.br");
+        request.setRequestURI("/robots.txt");
     }
 
     @Test
     public void devePermitirRobosComAFlagLigada() throws Exception {
-        assertThat(controller.robotsTxt(baseUrl, true),
+        assertThat(controller.robotsTxt(request, true),
                 stringContainsInOrder(asList("Sitemap", "http://servicos.gov.br/sitemap.xml", "User-agent", "*", "Disallow:")));
     }
 
     @Test
     public void deveBanirRobosComAFlagDesligada() throws Exception {
-        assertThat(controller.robotsTxt(baseUrl, false),
+        assertThat(controller.robotsTxt(request, false),
                 stringContainsInOrder(asList("User-agent", "*", "Disallow: /")));
     }
 }
