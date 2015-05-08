3f5550aece537c4f58f1a5d92807bbe0ca394f45
Jean Kirchner <jean.kirchner@gmail.com>
2015-05-08 16:05:34 -0300

    Corrige teste - #221

--- src/test/java/br/gov/servicos/indexador/IndexadorConteudoTest.java
+++ src/test/java/br/gov/servicos/indexador/IndexadorConteudoTest.java
@@ -1,6 +1,6 @@
 package br.gov.servicos.indexador;
 
-import br.gov.servicos.cms.ConteudoHtml;
+import br.gov.servicos.cms.Conteudo;
 import br.gov.servicos.cms.ConteudoRepository;
 import br.gov.servicos.fixtures.TestData;
 import br.gov.servicos.orgao.OrgaoRepository;
@@ -52,15 +52,15 @@ public class IndexadorConteudoTest {
         ArgumentCaptor<List> captor = ArgumentCaptor.forClass(List.class);
         verify(conteudoRepository).save(captor.capture());
 
-        List<ConteudoHtml> conteudos = captor.getValue();
-        ConteudoHtml arquivoNacional = conteudos.get(0);
+        List<Conteudo> conteudos = captor.getValue();
+        Conteudo arquivoNacional = conteudos.get(0);
 
         Assert.assertThat(arquivoNacional.getTitulo(), equalTo("Arquivo Nacional"));
-        Assert.assertThat(arquivoNacional.getHtml(), containsString("Órgão central do Sistema de Gestão de Documentos de Arquivos - SIGA da Administração Pública Federal, tem por finalidade implementar e acompanhar a Política Nacional de Arquivos, por meio da gestão, do recolhimento, do tratamento técnico, da preservação e da divulgação do patrimônio documental do País, garantindo pleno acesso à informação, visando apoiar as decisões governamentais de caráter político-administrativo, o cidadão na defesa de seus direitos e de incentivar a produção de conhecimento científico e cultural"));
+        Assert.assertThat(arquivoNacional.getConteudo(), containsString("Órgão central do Sistema de Gestão de Documentos de Arquivos - SIGA da Administração Pública Federal, tem por finalidade implementar e acompanhar a Política Nacional de Arquivos, por meio da gestão, do recolhimento, do tratamento técnico, da preservação e da divulgação do patrimônio documental do País, garantindo pleno acesso à informação, visando apoiar as decisões governamentais de caráter político-administrativo, o cidadão na defesa de seus direitos e de incentivar a produção de conhecimento científico e cultural"));
 
-        ConteudoHtml bancoCentral = conteudos.get(1);
+        Conteudo bancoCentral = conteudos.get(1);
         Assert.assertThat(bancoCentral.getTitulo(), equalTo("Banco Central do Brasil"));
-        Assert.assertThat(bancoCentral.getHtml(), containsString("O Banco Central do Brasil foi criado pela [Lei 4.595], de 31 de dezembro de 1964. É o principal executor das orientações do Conselho Monetário Nacional e responsável por garantir o poder de compra da moeda nacional, tendo por objetivos:"));
+        Assert.assertThat(bancoCentral.getConteudo(), containsString("O Banco Central do Brasil foi criado pela [Lei 4.595], de 31 de dezembro de 1964. É o principal executor das orientações do Conselho Monetário Nacional e responsável por garantir o poder de compra da moeda nacional, tendo por objetivos:"));
     }
 
 }
