862e15110184092161aed4029e773d48a7c3d208
Erick Pintor <erickpintor@gmail.com>
2015-05-08 17:03:31 -0300

    Corrige parse de unicode de CSS - #251
    Baseado no patch: https://code.google.com/p/ie7-js/issues/detail?id=254

--- src/main/resources/static/assets/IE9.js
+++ src/main/resources/static/assets/IE9.js
@@ -422,7 +422,8 @@ var Parser = RegGrp.extend({ignoreCase: true});
 var SINGLE_QUOTES       = /'/g,
     ESCAPED             = /'(\d+)'/g,
     ESCAPE              = /\\/g,
-    UNESCAPE            = /\\([nrtf'"])/g;
+    UNESCAPE            = /\\([nrtf'"])/g
+    UNICODE             = /\\([\da-fA-F]{1,4})/g;
 
 var strings = [];
 
@@ -451,6 +452,9 @@ function decode(query) {
 function encodeString(string) {
   var index = strings.length;
   strings[index] = string.slice(1, -1)
+    .replace(UNICODE, function(match, chr) {
+      return eval("'\\u" + "0000".slice(chr.length) + chr + "'");
+    })
     .replace(UNESCAPE, "$1")
     .replace(SINGLE_QUOTES, "\\'");
   return "'" + index + "'";
