24939fdfe1801610dcecfd0fd1e4545ada76907f
Carlos Villela <cvillela@thoughtworks.com>
2015-05-14 16:28:29 -0300

    Resolve problemas com findbugs - #257

--- src/test-integration/java/br/gov/servicos/config/ValidadorYamlTest.java
+++ src/test-integration/java/br/gov/servicos/config/ValidadorYamlTest.java
@@ -1,16 +1,24 @@
 package br.gov.servicos.config;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStreamReader;
 
 public class ValidadorYamlTest {
 
+    private Yaml yaml;
+
+    @Before
+    public void setUp() throws Exception {
+        yaml = new Yaml();
+    }
+
     @Test
     public void deveSerVálido() throws Exception {
-        new org.yaml.snakeyaml.Yaml().parse(
-                new InputStreamReader(
-                        new ClassPathResource("file:src/main/resources/application.yaml").getInputStream()));
+        ClassPathResource resource = new ClassPathResource("file:src/main/resources/application.yaml");
+        yaml.parse(new InputStreamReader(resource.getInputStream()));
     }
 }
