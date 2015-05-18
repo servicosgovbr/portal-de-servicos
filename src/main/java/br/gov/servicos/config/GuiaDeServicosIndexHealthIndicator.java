d74e6750fa9b845164c20517cc0832eda11e010d
Carlos Villela <cvillela@thoughtworks.com>
2015-05-18 16:41:01 -0300

    Adiciona indicador de saúde para os índices do ES

---
+++ src/main/java/br/gov/servicos/config/GuiaDeServicosIndexHealthIndicator.java
@@ -0,0 +1,51 @@
+package br.gov.servicos.config;
+
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Component;
+
+import static br.gov.servicos.config.GuiaDeServicosIndex.GDS_IMPORTADOR;
+import static br.gov.servicos.config.GuiaDeServicosIndex.GDS_PERSISTENTE;
+import static java.lang.String.format;
+import static lombok.AccessLevel.PRIVATE;
+
+@Component
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+public class GuiaDeServicosIndexHealthIndicator implements HealthIndicator {
+
+    ElasticsearchTemplate es;
+
+    @Autowired
+    public GuiaDeServicosIndexHealthIndicator(ElasticsearchTemplate es) {
+        this.es = es;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder health = Health.unknown();
+
+        health = indice(health, GDS_IMPORTADOR);
+        health = indice(health, GDS_PERSISTENTE);
+
+        return health.build();
+    }
+
+    private Health.Builder indice(Health.Builder health, String indice) {
+        try {
+
+            if (es.indexExists(indice)) {
+                long count = es.count(new NativeSearchQueryBuilder().withIndices(indice).build());
+                health = health.up().withDetail(indice, format("ok (%d docs)", count));
+            } else {
+                health = health.down().withDetail(indice, "missing");
+            }
+            return health;
+        } catch (Exception e) {
+            return Health.down().withDetail(indice, "exception").withException(e);
+        }
+    }
+}
---
+++ src/test/java/br/gov/servicos/config/GuiaDeServicosIndexHealthIndicatorTest.java
@@ -0,0 +1,58 @@
+package br.gov.servicos.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+
+import static br.gov.servicos.config.GuiaDeServicosIndex.GDS_IMPORTADOR;
+import static br.gov.servicos.config.GuiaDeServicosIndex.GDS_PERSISTENTE;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GuiaDeServicosIndexHealthIndicatorTest {
+
+    @Mock
+    ElasticsearchTemplate es;
+
+    GuiaDeServicosIndexHealthIndicator indicator;
+
+    @Before
+    public void setUp() throws Exception {
+        indicator = new GuiaDeServicosIndexHealthIndicator(es);
+    }
+
+    @Test
+    public void retornaUpParaAmbosIndicesCriados() throws Exception {
+        given(es.count(any(SearchQuery.class))).willReturn(42L);
+
+        given(es.indexExists(GDS_IMPORTADOR)).willReturn(true);
+        given(es.indexExists(GDS_PERSISTENTE)).willReturn(true);
+
+        Health health = indicator.health();
+
+        assertThat(health.getStatus(), is(Status.UP));
+        assertThat(health.getDetails().get(GDS_IMPORTADOR), is("ok (42 docs)"));
+        assertThat(health.getDetails().get(GDS_PERSISTENTE), is("ok (42 docs)"));
+    }
+
+    @Test
+    public void retornaDownParaAmbosIndicesNaoCriados() throws Exception {
+        given(es.indexExists(GDS_IMPORTADOR)).willReturn(false);
+        given(es.indexExists(GDS_PERSISTENTE)).willReturn(false);
+
+        Health health = indicator.health();
+
+        assertThat(health.getStatus(), is(Status.DOWN));
+        assertThat(health.getDetails().get(GDS_IMPORTADOR), is("missing"));
+        assertThat(health.getDetails().get(GDS_PERSISTENTE), is("missing"));
+    }
+}
