63ff1abc9bb28b13bdf41516cd9710c9295ac52a
Carlos Villela <cvillela@thoughtworks.com>
2015-05-21 12:50:55 -0300

    Adiciona testes para casos de falha

--- src/test/java/br/gov/servicos/cms/MarkdownTest.java
+++ src/test/java/br/gov/servicos/cms/MarkdownTest.java
@@ -1,18 +1,29 @@
 package br.gov.servicos.cms;
 
+import br.gov.servicos.foundation.exceptions.ConteudoNaoEncontrado;
 import lombok.experimental.FieldDefaults;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 import static lombok.AccessLevel.PRIVATE;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @FieldDefaults(level = PRIVATE)
+@RunWith(MockitoJUnitRunner.class)
 public class MarkdownTest {
 
     Markdown markdown;
+
+    @Mock
+    ClassPathResource resource;
 
     @Before
     public void setUp() {
@@ -20,14 +31,25 @@ public class MarkdownTest {
     }
 
     @Test
-    public void geraHtmlDeResource() {
+    public void deveGerarHtmlDeResource() {
         assertThat(markdown.toHtml(new ClassPathResource("/conteudo/pagina.md")).getHtml(),
                 containsString("<h1>Uma Página Qualquer</h1>"));
     }
 
     @Test
-    public void extraiTituloDaPaginaEmMarkdown() {
+    public void deveExtrairTituloDaPaginaEmMarkdown() {
         assertThat(markdown.toHtml(new ClassPathResource("/conteudo/pagina.md")).getTitulo(),
                 containsString("Uma Página Qualquer"));
+    }
+
+    @Test(expected = ConteudoNaoEncontrado.class)
+    public void deveJogarExcecaoQuandoConteudoNaoExiste() {
+        markdown.toHtml(new ClassPathResource("/conteudo/404.md"));
+    }
+
+    @Test(expected = ConteudoNaoEncontrado.class)
+    public void deveJogarExcecaoQuandoConteudoNaoPodeSerAcessado() throws Exception {
+        given(resource.getInputStream()).willThrow(new IOException("mock"));
+        markdown.toHtml(resource);
     }
 }
