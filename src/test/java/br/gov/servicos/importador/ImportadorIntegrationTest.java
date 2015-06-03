ce6b2d5f98d0441e4385542aeac260bc69b57ad1
Carlos Villela <cvillela@thoughtworks.com>
2015-06-03 02:27:15 -0300

    Adiciona teste para garantir que o importador não quebrou campos CDATA - #173

--- src/test/java/br/gov/servicos/importador/ImportadorIntegrationTest.java
+++ src/test/java/br/gov/servicos/importador/ImportadorIntegrationTest.java
@@ -1,6 +1,7 @@
 package br.gov.servicos.importador;
 
 import br.gov.servicos.Main;
+import br.gov.servicos.servico.ServicoRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.junit.Test;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
@@ -19,8 +23,13 @@ public class ImportadorIntegrationTest {
     @Autowired
     Importador importador;
 
+    @Autowired
+    ServicoRepository servicos;
+
     @Test
     public void deveRodarImportador() throws Exception {
         importador.importar();
+
+        assertThat(servicos.findOne("passaporte").getDescricao().split("\n").length, is(28));
     }
 }
