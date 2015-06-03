e7436d06502b5db5cffe33364149939700b1dbe6
Carlos Villela <cvillela@thoughtworks.com>
2015-06-03 03:32:05 -0300

    Usa um set, deixa de filtrar links começados com localhost - #173

--- src/test/java/br/gov/servicos/xml/ValidadorLinksIntegrationTest.java
+++ src/test/java/br/gov/servicos/xml/ValidadorLinksIntegrationTest.java
@@ -3,8 +3,6 @@ package br.gov.servicos.xml;
 import br.gov.servicos.testutil.ParallelizedParameterized;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.experimental.FieldDefaults;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,8 +14,9 @@ import java.util.Collection;
 import java.util.stream.Stream;
 
 import static br.gov.servicos.foundation.IO.read;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static lombok.AccessLevel.PRIVATE;
+import static org.jsoup.Jsoup.parse;
 
 @RunWith(ParallelizedParameterized.class)
 @FieldDefaults(level = PRIVATE, makeFinal = true)
@@ -26,14 +25,13 @@ public class ValidadorLinksIntegrationTest {
     @Parameterized.Parameters(name = "{0}:{1}")
     public static Collection<Object[]> data() throws IOException {
         return Stream.of(new PathMatchingResourcePatternResolver().getResources("file:src/main/resources/v1/*.xml"))
-                .flatMap(r -> {
-                    Document doc = Jsoup.parse(read(r));
-                    return doc.select("link")
-                            .stream()
-                            .filter(e -> !e.attr("href").startsWith("http://localhost"))
-                            .map(e -> new Object[]{r.getFilename(), e.attr("href")});
-                })
-                .collect(toList());
+                .flatMap(r -> parse(read(r))
+                        .select("link")
+                        .stream()
+                        .map(e -> new Object[]{
+                                r.getFilename(),
+                                e.attr("href")}))
+                .collect(toSet());
     }
 
     String url;
