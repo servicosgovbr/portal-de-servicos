2ba92a1f00ddec1d5342d2ef1408114660f9b7ed
Carlos Villela <cvillela@thoughtworks.com>
2015-06-03 03:14:24 -0300

    Organiza imports - #173

--- src/test/java/br/gov/servicos/xml/ValidadorLinksIntegrationTest.java
+++ src/test/java/br/gov/servicos/xml/ValidadorLinksIntegrationTest.java
@@ -5,32 +5,19 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.experimental.FieldDefaults;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.Collection;
 import java.util.stream.Stream;
 
 import static br.gov.servicos.foundation.IO.read;
 import static java.util.stream.Collectors.toList;
 import static lombok.AccessLevel.PRIVATE;
-import static org.junit.Assert.fail;
 
 @RunWith(ParallelizedParameterized.class)
 @FieldDefaults(level = PRIVATE, makeFinal = true)
