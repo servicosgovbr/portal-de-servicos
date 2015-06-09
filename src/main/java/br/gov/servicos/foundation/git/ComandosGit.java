ac8e3ce13bd042337117092dc7d0a1cf3ea2dfca
Erick Pintor <erickpintor@gmail.com>
2015-06-09 17:20:07 -0300

    Faz importador falhar caso haja erros ao clonar o repositorio - #399

--- src/main/java/br/gov/servicos/foundation/git/ComandosGit.java
+++ src/main/java/br/gov/servicos/foundation/git/ComandosGit.java
@@ -1,46 +1,41 @@
 package br.gov.servicos.foundation.git;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.SneakyThrows;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.PullResult;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 
-import static java.lang.String.format;
 import static org.eclipse.jgit.merge.MergeStrategy.THEIRS;
 
-@Slf4j
 @Component
 public class ComandosGit {
 
-    public void clonaOuAtualizaRepositorio(String url, String caminhoLocal) {
+    public String clonaOuAtualizaRepositorio(String url, String caminhoLocal) {
         File diretorioLocal = new File(caminhoLocal);
 
-        try(Git repositorio = Git.open(diretorioLocal)) {
-            pull(repositorio);
+        try (Git repositorio = Git.open(diretorioLocal)) {
+            return pull(repositorio);
         } catch (IOException e) {
-            clone(url, diretorioLocal);
+            return clone(url, diretorioLocal);
         }
     }
 
-    private void pull(Git repositorio) {
-        try {
-            repositorio.pull().setStrategy(THEIRS).call();
-        } catch (GitAPIException e) {
-            log.error(format("Erro ao atualizar repositório %s", repositorio), e);
-        }
+    @SneakyThrows
+    private String pull(Git repositorio) {
+        PullResult pull = repositorio.pull().setStrategy(THEIRS).call();
+        return pull.getMergeResult().getNewHead().name();
     }
 
-    private void clone(String url, File diretorioLocal) {
-        CloneCommand clone = Git.cloneRepository().setDirectory(diretorioLocal);
+    @SneakyThrows
+    private String clone(String url, File diretorioLocal) {
+        CloneCommand clone = Git.cloneRepository().setURI(url).setDirectory(diretorioLocal);
 
         try (Git repositorio = clone.call()) {
-            pull(repositorio);
-        } catch (GitAPIException e) {
-            log.error(format("Erro ao importar repositório %s para %s", url, diretorioLocal), e);
+            return pull(repositorio);
         }
     }
 
