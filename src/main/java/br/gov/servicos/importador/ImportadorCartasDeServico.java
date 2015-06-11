dcb1a696089144194b6857f7e398be2dd5a78391
Erick Pintor <erickpintor@gmail.com>
2015-06-11 19:37:24 -0300

    Simplifica clone do repositório de cartas - #399

--- src/main/java/br/gov/servicos/importador/ImportadorCartasDeServico.java
+++ src/main/java/br/gov/servicos/importador/ImportadorCartasDeServico.java
@@ -1,8 +1,10 @@
 package br.gov.servicos.importador;
 
-import br.gov.servicos.foundation.git.ComandosGit;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,31 +18,25 @@ import static lombok.AccessLevel.PRIVATE;
 @Component
 class ImportadorCartasDeServico {
 
-    ComandosGit git;
     String urlRepositorio;
-    boolean deveImportar;
 
     @Autowired
-    ImportadorCartasDeServico(ComandosGit git,
-                              @Value("${pds.cartas.repositorio}") String urlRepositorio,
-                              @Value("${flags.importar.cartas}") Boolean deveImportar) {
-
-        this.git = git;
-        this.deveImportar = deveImportar;
+    ImportadorCartasDeServico(@Value("${pds.cartas.repositorio}") String urlRepositorio) {
         this.urlRepositorio = urlRepositorio;
     }
 
+    @SneakyThrows
     String importar(File caminhoLocal) {
-        if (!deveImportar) {
-            log.info("Importação de cartas de servico desligada (FLAGS_IMPORTAR_CARTAS_DE_SERVICO=false)");
-            return null;
-        }
-
         log.info("Importando cartas de serviço de {} para {}", urlRepositorio, caminhoLocal);
-        String versaoRepositorio = git.clonaRepositorio(urlRepositorio, caminhoLocal);
+        CloneCommand clone = Git.cloneRepository()
+                .setURI(urlRepositorio)
+                .setDirectory(caminhoLocal);
 
-        log.info("Repositório de cartas de serviço clonado na versão {}", versaoRepositorio);
-        return versaoRepositorio;
+        try (Git repositorio = clone.call()) {
+            String head = repositorio.log().call().iterator().next().getName();
+            log.info("Repositório de cartas de serviço clonado na versão {}", head);
+            return head;
+        }
     }
 
 }
