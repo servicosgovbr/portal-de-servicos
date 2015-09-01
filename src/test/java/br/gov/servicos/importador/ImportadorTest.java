f89b529f2659b213b75cc11d68d4a1ff9657e01a
Carlos Villela <cvillela@thoughtworks.com>
2015-09-01 02:12:53 -0300

    Corrige ImportadorTest, remove ImportadorCartasDeServico - #443

--- src/test/java/br/gov/servicos/importador/ImportadorTest.java
+++ src/test/java/br/gov/servicos/importador/ImportadorTest.java
@@ -8,17 +8,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.File;
-
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ImportadorTest {
-
-    @Mock
-    ImportadorCartasDeServico importadorCartasDeServico;
 
     @Mock
     ImportadorV3 importadorV3;
@@ -31,12 +25,6 @@ public class ImportadorTest {
     @Before
     public void setUp() throws Exception {
         importador = new Importador(importadorV3, importadorConteudo);
-    }
-
-    @Test
-    public void deveRodarImportadorDeCartasDeServico() {
-        importador.importar();
-        verify(importadorCartasDeServico).importar(any(File.class));
     }
 
     @Test
