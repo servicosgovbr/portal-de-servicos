40f38c4ac74a4bb041127a951c953fad674537e0
Carlos Villela <cvillela@thoughtworks.com>
2015-09-02 13:06:17 -0300

    Remove AreaDeInteresseRepository, usando estrutura similar ao OrgaoController

--- src/main/java/br/gov/servicos/servico/ServicoRepository.java
+++ src/main/java/br/gov/servicos/servico/ServicoRepository.java
@@ -1,5 +1,6 @@
 package br.gov.servicos.servico;
 
+import br.gov.servicos.v3.schema.AreaDeInteresse;
 import br.gov.servicos.v3.schema.Orgao;
 import br.gov.servicos.v3.schema.Servico;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +16,13 @@ public interface ServicoRepository extends ElasticsearchRepository<Servico, Stri
         return findAll(new PageRequest(0, Integer.MAX_VALUE)).getContent()
                 .stream()
                 .filter(s -> s.getOrgao().getId().equals(orgao.getId()))
+                .collect(toList());
+    }
+
+    default List<Servico> findByAreaDeInteresse(AreaDeInteresse areaDeInteresse) {
+        return findAll(new PageRequest(0, Integer.MAX_VALUE)).getContent()
+                .stream()
+                .filter(s -> s.getAreasDeInteresse().contains(areaDeInteresse))
                 .collect(toList());
     }
 }
--- src/main/java/br/gov/servicos/servico/areaDeInteresse/AreaDeInteresseController.java
+++ src/main/java/br/gov/servicos/servico/areaDeInteresse/AreaDeInteresseController.java
@@ -1,7 +1,9 @@
 package br.gov.servicos.servico.areaDeInteresse;
 
 import br.gov.servicos.busca.Buscador;
+import br.gov.servicos.cms.Conteudo;
 import br.gov.servicos.cms.Markdown;
+import br.gov.servicos.servico.ServicoRepository;
 import br.gov.servicos.v3.schema.AreaDeInteresse;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static lombok.AccessLevel.PRIVATE;
 
@@ -22,23 +23,25 @@ import static lombok.AccessLevel.PRIVATE;
 class AreaDeInteresseController {
 
     Buscador buscador;
-    private AreaDeInteresseRepository areasDeInteresse;
+    ServicoRepository servicos;
     Markdown markdown;
 
     @Autowired
-    AreaDeInteresseController(Buscador buscador, AreaDeInteresseRepository areasDeInteresse, Markdown markdown) {
+    AreaDeInteresseController(Buscador buscador, ServicoRepository servicos, Markdown markdown) {
         this.buscador = buscador;
-        this.areasDeInteresse = areasDeInteresse;
+        this.servicos = servicos;
         this.markdown = markdown;
     }
 
     @RequestMapping("/area-de-interesse/{id}")
     ModelAndView areaDeInteresse(@PathVariable String id) {
         Map<String, Object> model = new HashMap<>();
+        AreaDeInteresse areaDeInteresse = AreaDeInteresse.findById(id);
 
-        model.put("areaDeInteresse", areasDeInteresse.get(id).get());
-        model.put("resultados", buscador.buscaConteudosPor("areasDeInteresse", ofNullable(AreaDeInteresse.findById(id).name()))
+        model.put("areaDeInteresse", areaDeInteresse);
+        model.put("resultados", servicos.findByAreaDeInteresse(areaDeInteresse)
                 .stream()
+                .map(Conteudo::fromServico)
                 .sorted((x, y) -> x.getId().compareTo(y.getId()))
                 .collect(toList()));
 
