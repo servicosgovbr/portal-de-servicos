cd36e96323dbd30d8673b69ad7db8142949c1ed9
Carlos Villela <cvillela@thoughtworks.com>
2015-09-11 03:28:33 -0300

    Usa anotacao correta

--- src/test/java/br/gov/servicos/frontend/FrontendIntegrationTest.java
+++ src/test/java/br/gov/servicos/frontend/FrontendIntegrationTest.java
@@ -4,8 +4,8 @@ import br.gov.servicos.Main;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
 @WebAppConfiguration
-@IntegrationTest("server.port:0")
+@WebIntegrationTest(randomPort = true)
 public class FrontendIntegrationTest {
 
     @Value("${local.server.port}")
