05c1b02b1b7fc9e545b35f3cd64ce23ae97ba3df
Carlos Villela <cvillela@thoughtworks.com>
2015-09-11 02:46:21 -0300

    Remove código morto

--- src/main/java/br/gov/servicos/cms/ConteudoRepository.java
+++ src/main/java/br/gov/servicos/cms/ConteudoRepository.java
@@ -1,17 +1,6 @@
 package br.gov.servicos.cms;
 
-import org.elasticsearch.index.query.TermFilterBuilder;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-
 public interface ConteudoRepository extends ElasticsearchRepository<Conteudo, String> {
-
-    default List<Conteudo> findByTipo(String tipoConteudo) {
-        return search(new NativeSearchQueryBuilder()
-                .withFilter(new TermFilterBuilder("tipoConteudo", tipoConteudo))
-                .build())
-                .getContent();
-    }
 }
