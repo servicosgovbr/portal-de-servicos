30a0d344a509d147f0d96e5242ee4b06e7e870e5
Carlos Villela <cvillela@thoughtworks.com>
2015-09-11 04:31:23 -0300

    Adiciona URL para miolo de página de conteúdo - #448

--- src/main/java/br/gov/servicos/cms/ConteudoController.java
+++ src/main/java/br/gov/servicos/cms/ConteudoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static lombok.AccessLevel.PRIVATE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @FieldDefaults(level = PRIVATE, makeFinal = true)
@@ -15,6 +16,11 @@ class ConteudoController {
     @RequestMapping("/conteudo/{id}")
     ModelAndView conteudo(@PathVariable("id") ConteudoHtml conteudo) {
         return new ModelAndView("conteudo", "conteudo", conteudo);
+    }
+
+    @RequestMapping(value = "/miolo/conteudo/{id}", method = GET)
+    ModelAndView getHtml(@PathVariable("id") ConteudoHtml conteudo) {
+        return new ModelAndView("conteudo :: //section", conteudo(conteudo).getModel());
     }
 
 }
