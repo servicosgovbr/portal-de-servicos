1a1241a532526777984b4a7f5660d6903c46df43
Samantha Rosa <sah.rosa@gmail.com>
2015-09-15 16:07:12 -0300

    ajustando visualização de canais de prestação #447

--- src/main/java/br/gov/servicos/v3/schema/TipoDeCanalDePrestacao.java
+++ src/main/java/br/gov/servicos/v3/schema/TipoDeCanalDePrestacao.java
@@ -12,65 +12,67 @@ import javax.xml.bind.annotation.XmlType;
 public enum TipoDeCanalDePrestacao {
 
     @XmlEnumValue("aplicativo-movel")
-    APLICATIVO_MOVEL("Aplicativo móvel", false),
+    APLICATIVO_MOVEL("Aplicativo móvel", false, ""),
 
     @XmlEnumValue("e-mail")
-    E_MAIL("E-mail", false),
+    E_MAIL("E-mail", false, ""),
 
     @XmlEnumValue("fax")
-    FAX("Fax", false),
+    FAX("Fax", false, ""),
 
     @XmlEnumValue("postal")
-    POSTAL("Postal", false),
+    POSTAL("Postal", false, ""),
 
     @XmlEnumValue("presencial")
-    PRESENCIAL("Presencial", false),
+    PRESENCIAL("Presencial", false, ""),
 
     @XmlEnumValue("sms")
-    SMS("SMS", false),
+    SMS("SMS", false, ""),
 
     @XmlEnumValue("telefone")
-    TELEFONE("Telefone", false),
+    TELEFONE("Telefone", false, ""),
 
     @XmlEnumValue("web")
-    WEB("Acessar", true),
+    WEB("Web", true, "Acesse o site"),
 
     @XmlEnumValue("web-acompanhar")
-    WEB_ACOMPANHAR("Acompanhar", true),
+    WEB_ACOMPANHAR("Web", true, "Acompanhe esta etapa do serviço"),
 
     @XmlEnumValue("web-agendar")
-    WEB_AGENDAR("Agendar", true),
+    WEB_AGENDAR("Web", true, "Agende esta etapa do serviço"),
 
     @XmlEnumValue("web-calcular-taxas")
-    WEB_CALCULAR_TAXAS("Calcular taxas", true),
+    WEB_CALCULAR_TAXAS("Web", true, "Calcule as taxas desta etapa"),
 
     @XmlEnumValue("web-consultar")
-    WEB_CONSULTAR("Consultar", true),
+    WEB_CONSULTAR("Web", true, "Realize uma consulta nesta etapa"),
 
     @XmlEnumValue("web-declarar")
-    WEB_DECLARAR("Declarar", true),
+    WEB_DECLARAR("Web", true, "Realize uma declaração nesta etapa"),
 
     @XmlEnumValue("web-emitir")
-    WEB_EMITIR("Emitir", true),
+    WEB_EMITIR("Web", true, "Faça a emissão para esta etapa"),
 
     @XmlEnumValue("web-inscrever-se")
-    WEB_INSCREVER_SE("Inscrever-se", true),
+    WEB_INSCREVER_SE("Web", true, "Inscreva-se"),
 
     @XmlEnumValue("web-postos-de-atendimento")
-    WEB_POSTOS_DE_ATENDIMENTO("Postos de atendimento", true),
+    WEB_POSTOS_DE_ATENDIMENTO("Web", true, "Conheça os postos de atendimento"),
 
     @XmlEnumValue("web-preencher")
-    WEB_PREENCHER("Preencher", true),
+    WEB_PREENCHER("Web", true, "Preencha esta etapa do serviço"),
 
     @XmlEnumValue("web-simular")
-    WEB_SIMULAR("Simular", true);
+    WEB_SIMULAR("Web", true, "Simule esta etapa do serviço");
 
     private final String value;
     private final boolean destacado;
+    private final String descricaoLink;
 
-    TipoDeCanalDePrestacao(String value, boolean destacado) {
+    TipoDeCanalDePrestacao(String value, boolean destacado, String descricaoLink) {
         this.value = value;
         this.destacado = destacado;
+        this.descricaoLink = descricaoLink;
     }
 
 }
