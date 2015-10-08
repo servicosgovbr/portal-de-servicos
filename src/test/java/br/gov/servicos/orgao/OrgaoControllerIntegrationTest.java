59eff3b3652b8e38897e3882cbb46efbaadd5acf
Jean Kirchner <jeankirchner@gmail.com>
2015-10-07 22:39:47 -0300

    limpa base antes de rodar teste

--- src/test/java/br/gov/servicos/orgao/OrgaoControllerIntegrationTest.java
+++ src/test/java/br/gov/servicos/orgao/OrgaoControllerIntegrationTest.java
@@ -1,13 +1,17 @@
 package br.gov.servicos.orgao;
 
 import br.gov.servicos.Main;
+import br.gov.servicos.config.PortalDeServicosIndex;
 import br.gov.servicos.v3.schema.Orgao;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
@@ -19,6 +23,14 @@ public class OrgaoControllerIntegrationTest {
 
     @Autowired
     OrgaoController orgaoController;
+
+    @Autowired
+    PortalDeServicosIndex index;
+
+    @Before
+    public void setup() throws IOException {
+        index.recriar();
+    }
 
     @Test
     public void deveEncontrarConteudoParaTodosOrgaosNoRepositorio() throws Exception {
